2a641a2e280aae42506e01d5c544fe8928004748
benmulpeter <benmulpeter@me.com>
2013-12-24 20:03:04 +0000

    First file

---
+++ periodic_table.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string element;
+  cout << "Please enter the element name: ";
+  cin >> element;
+
+  if (element == "Helium") {
+    cout << "Symbol: He" << endl;
+  } else {
+    if (element == "Hydrogen") {
+      cout << "Symbol: H" << endl;
+    }else {
+      cout << "Invalid element" << endl;
+    }
+  }
+  return 0;
+}
